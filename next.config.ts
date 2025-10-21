35c7aec88a588af42f3b53e1361c340087aa8fae
Kyle Klus <kyle.klus@f-i.de>
2025-10-21 19:10:56 +0200

    WIP: Rendered first md page

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-/**
- * @format
- */
+import createMDX from '@next/mdx'
 
 import type { NextConfig } from "next";
+import remarkGfm from 'remark-gfm'
 
 const nextConfig: NextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
   basePath: '',
   output: 'export',
@@ -14,4 +14,15 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+  options: {
+    remarkPlugins: [
+      // Without options
+      'remark-gfm',
+    ],
+  },
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
